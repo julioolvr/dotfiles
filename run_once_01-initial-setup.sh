7b25ee53ee467495ec4fcef5c11b543f0b908496
Julio <julio.olvr@gmail.com>
2022-01-30 01:04:19 +0100

    Migrate to bash installer for Homebrew

--- run_once_01-initial-setup.sh
+++ run_once_01-initial-setup.sh
@@ -3,4 +3,4 @@ echo "Installing XCode command line tools..."
 xcode-select --install
 
 echo "Installing Homebrew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
